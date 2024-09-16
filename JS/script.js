e0ad03a062ac9b631169f67e056c08aaded34ec3
M Naufal Azmi <mnapintar25@gmail.com>
2024-09-17 00:14:53 +0800

    revise

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,45 @@
-// ini javascript
+// Script JS
 
-// Perhitungan Luas Segitiga
-function calcluas() {
-    let alas = document.getElementById("alas").value;
-    let tinggi = document.getElementById("tinggi").value;
-    
-    if (isNaN(alas) || isNaN(tinggi) || alas <= 0 || tinggi <= 0) {
-        alert("Masukkan nilai angka yang valid untuk alas dan tinggi.");
+function hitungluassegitiga() {
+    let alas = parseFloat(document.getElementById("alas").value);
+    let tinggi = parseFloat(document.getElementById("tinggi").value);
+
+    if  (isNaN(alas) || isNaN(tinggi) || alas <= 0 || tinggi <= 0) {
+        alert ("harap masukkan semua nilai dengan benar");
         return;
     }
 
     let luas = 0.5 * alas * tinggi;
 
-    document.getElementById("hasil").innerHTML = "Luas Segitiga: " + luas + " satuan luas.";
+    document.getElementById("hasil").innerHTML = "Luas Segitiga: " + luas +  " cm.";
 }
 
 
-// Perhitungan keliling segitiga
-function calckeliling() {
-    let sisi1 = parseFloat(document.getElementById('sisi1').value);
-    let sisi2 = parseFloat(document.getElementById('sisi2').value);
-    let sisi3 = parseFloat(document.getElementById('sisi3').value);
+function resetluassegitiga() {
+    document.getElementById("alas").value = "";
+    document.getElementById("tinggi").value = "";
+    document.getElementById("hasil").innerHTML = "";
+}
 
-    if ( isNaN(sisi1) || isNaN(sisi2) || isNaN(sisi3)) {
-        alert('Harap masukkan semua nilai dengan benar.');
+
+
+function hitungkelilingsegitiga() {
+    let sisi1 = parseFloat(document.getElementById("sisi1").value);
+    let sisi2 = parseFloat(document.getElementById("sisi2").value);
+    let sisi3 = parseFloat(document.getElementById("sisi3").value);
+
+    if (isNaN(sisi1) || isNaN(sisi2) || isNaN(sisi3) || sisi1 <= 0 || sisi2 <= 0 || sisi3 <= 0) {
+        alert("Harap masukkan semua nilai dengan benar");
         return;
     }
 
     let keliling = sisi1 + sisi2 + sisi3;
-
-    document.getElementById('hasil-keliling').textContent = `K = s + s + s = ${keliling}`;
+    document.getElementById("hasil-keliling").innerHTML =  keliling + " cm";
 }
 
-// reset perhitungan
-function resetluas() {
-    document.getElementById('alas').value = '';
-    document.getElementById('tinggi').value = '';
-    document.getElementById('hasil-luas').textContent = '0';
-}
-
-function resetkeliling() {
-    document.getElementById('sisi1').value = '';
-    document.getElementById('sisi2').value = '';
-    document.getElementById('sisi3').value = '';
-    document.getElementById('hasil-keliling').textContent = '0';
+function resetkelilingsegitiga() {
+    document.getElementById("sisi1").value = "";
+    document.getElementById("sisi2").value = "";
+    document.getElementById("sisi3").value = "";
+    document.getElementById("hasil-keliling").innerHTML = "Hasil Perhitungan:";
 }
